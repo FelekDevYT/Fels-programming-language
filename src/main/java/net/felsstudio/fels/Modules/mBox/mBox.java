f435a4548b972f18cf35e4215c8b1f0852337b73
felek <felek@DESKTOP-8AIIM5Q>
2024-08-09 08:43:06 +0300

    Refactor mBox to javaFX add javaFX

--- src/main/java/net/felsstudio/fels/Modules/mBox/mBox.java
+++ src/main/java/net/felsstudio/fels/Modules/mBox/mBox.java
@@ -1,33 +1,48 @@
 package main.java.net.felsstudio.fels.Modules.mBox;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import net.felsstudio.fels.Modules.Module;
 import main.java.net.felsstudio.fels.lib.*;
 import javax.swing.JOptionPane;
+import java.util.Optional;
 
 public class mBox implements Module {
     @Override
     public void init() {
-        Functions.set("showErrorMessage", args ->{
-        JOptionPane.showMessageDialog(null,args[0], String.valueOf(args[1]),JOptionPane.ERROR_MESSAGE);
+    Functions.set("showErrorMessage", args ->{
+        Alert dialog = new Alert(Alert.AlertType.ERROR);
+        dialog.setTitle(args[0].asString());
+        dialog.setHeaderText(null);
+        dialog.setContentText(args[1].asString());
+        dialog.showAndWait();
         return NumberValue.ZERO;
     });
-        Functions.set("showInformationMessage",args ->{
-        JOptionPane.showMessageDialog(null,args[0], String.valueOf(args[1]),JOptionPane.INFORMATION_MESSAGE);
+    Functions.set("showInformationMessage",args ->{
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.setTitle(args[0].asString());
+        dialog.setHeaderText(null);
+        dialog.setContentText(args[1].asString());
+        dialog.showAndWait();
+            return NumberValue.ZERO;
+        });
+    Functions.set("showWarningMessage",args -> {
+        Alert dialog = new Alert(Alert.AlertType.WARNING);
+        dialog.setTitle(args[0].asString());
+        dialog.setHeaderText(null);
+        dialog.setContentText(args[1].asString());
+        dialog.showAndWait();
         return NumberValue.ZERO;
     });
-        Functions.set("showQuestionMessage",args ->{
-        JOptionPane.showMessageDialog(null,args[0], String.valueOf(args[1]),JOptionPane.QUESTION_MESSAGE);
+    Functions.set("showNoneMessage",args -> {
+        Alert dialog = new Alert(Alert.AlertType.NONE);
+        dialog.setTitle(args[0].asString());
+        dialog.setHeaderText(null);
+        dialog.setContentText(args[1].asString());
+        dialog.showAndWait();
         return NumberValue.ZERO;
     });
-        Functions.set("showWarningMessage",args -> {
-        JOptionPane.showMessageDialog(null,args[0], String.valueOf(args[1]),JOptionPane.WARNING_MESSAGE);
-        return NumberValue.ZERO;
-    });
-        Functions.set("showPlanMessage",args -> {
-        JOptionPane.showMessageDialog(null,args[0], String.valueOf(args[1]),JOptionPane.PLAIN_MESSAGE);
-        return NumberValue.ZERO;
-    });
-        Functions.set("showInputDialog",args -> {
+    Functions.set("showInputDialog",args -> {
         return new StringValue(JOptionPane.showInputDialog(null,args[0], String.valueOf(args[1]),JOptionPane.QUESTION_MESSAGE));
     });
     }
