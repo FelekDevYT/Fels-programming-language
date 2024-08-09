094d094342f709366917efa07e24ded459c0290e
felek <felek@DESKTOP-8AIIM5Q>
2024-08-09 19:15:24 +0300

    Update Information.java file

--- src/main/java/net/felsstudio/fels/lib/Information.java
+++ src/main/java/net/felsstudio/fels/lib/Information.java
@@ -6,10 +6,10 @@ import java.util.Date;
 
 public class Information {
     @INFO
-    public static final String FELS_VERSION = "0.1";
+    public static final String FELS_VERSION = "3 PRE-RELEASE";
 
     @INFO
-    public static final String FELS_AUTHOR = "Felek_,FelsStudio";
+    public static final String FELS_AUTHOR = "FelekDevYT,FelsStudio";
 
     @INFO
     public static final String DATE = new Date().toString();
